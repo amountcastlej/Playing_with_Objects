444e6027f4d0b1ba7be63fdcfd5a4819d7ea3f75
Adam Joines <amountcastlej@gmail.com>
2021-01-31 17:34:24 -0500

    JS added

---
+++ object.js
@@ -0,0 +1,26 @@
+// var users = [
+//     {
+//         name: "Michael",
+//         age: 37
+//     },
+//     {
+//         name: "John",
+//         age: 30
+//     },
+//     {
+//         name: "David",
+//         age: 27
+//     }
+// ];
+
+// How would you print/log John's age?
+// console.log(users[1].age)
+
+// How would you pirnt/log the name of the first object?
+// console.log(users[0].name)
+
+// How would you print/log the name and age of each user after using a for loop? 
+// for (let idx of users){
+//     console.log(idx.name, "-" ,idx.age);
+// }
+
